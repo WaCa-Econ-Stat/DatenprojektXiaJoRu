e82229fe27f764095168b4a2157afe581bff125b
WaCa-Econ-Stat <Wallner.Ca@campus.lmu.de>
2024-11-22 20:25:20 +0100

    Änderung

--- File1.R
+++ File1.R
@@ -1,3 +1,14 @@
 x <- matrix(1:9,ncol=3)
 apply(x, 1, function(z)  z[2]) #1. Zeile 2. Element
 
+Bino <- function(Alpha, k){
+  a <- Alpha
+  Produkt = 1
+  for(j in 1:k){
+    Produkt = prod(Produkt,
+                   (a+1-j) / j
+                   )
+  }
+  return(Produkt)
+}
+Bino(6,2)
