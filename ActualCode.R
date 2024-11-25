b959232d611cea7311149687ceda0b6f47c2d181
WaCa-Econ-Stat <Wallner.Ca@campus.lmu.de>
2024-11-25 22:12:34 +0100

    mit Regression

--- ActualCode.R
+++ ActualCode.R
@@ -85,3 +85,52 @@ Häufigkeiten <- apply(df7, 2, sum)
 barplot(Häufigkeiten, names.arg=Gründe2,
         col="lightblue")
 
+
+###2###
+###1
+Branchen <- factor(as.vector(data$branche), levels= 1:5, labels = c(
+  "Verarbeitendes Gewerbe", "Metall, Elektro, Fahrzeugbau ", "Handel, Verkehr, Nachrichten ",
+  "Unternehmensnahe DL, Finanz. ", "Information/Kommunikation "
+)) #enthält noch NA
+head(Branchen)
+Modell_Konstante <- lm(na.omit((data.frame(HO = as.vector(data$bhomeoff),B = Branchen))), formula=
+                         HO~B)
+summary(Modell_Konstante)   #Referenzkategorie ist hier das Verarbeitende Gewerbe
+
+###Modell ohne Konstante
+###2
+Durchschnitte <- sapply(as.vector( unique(Branchen)[1:5] ), function(z) mean(df8$data.bhomeoff[df8$Branchen == z]) )
+print(Durchschnitte)
+
+###3
+Modell <- lm(data=na.omit(data.frame(Größe = as.vector(data$size),
+                                     HO = as.vector(data$bhomeoff), Branchen)),
+             formula= HO ~ Größe+Branchen)
+stargazer(Modell, type="text") #Dummy wieder Verarbeitendes Gewerbe
+
+# Bei Infor sowie Handel bleibt es in etwas gleich, aber 
+#Metall erhöht es sich und bei DL verkleinert es sich, das liegt daran, dass
+#Metall: Davir (ohne Größe) war der Koeff ca. 0.05, dieser wurde aber zu hoch einge-
+#schätzt, weil Größe positiv mit Metall korrelliert und einen positiven Einfluss auf
+#Home Office hat (+ und +), damit wurde Metall mit 0.05 überschätzt. -> OmittedVariableBias
+Größe <- as.vector(data$size)
+Metall <- Branchen
+df8 <- na.omit( data.frame(Größe,Metall) )
+
+Br <- as.vector(unique(Branchen))[-length(unique(Branchen))]
+Br
+df8$Metall <- factor(df8$Metall, levels = Br, labels = c(rep(0,2),1,rep(0,2)))
+mean(df8$Größe[df8$Metall==0]) #Durchschnittsgröße nicht Metall 
+mean(df8$Größe[df8$Metall==1]) #Durchschnittsgröße Metall > als von nicht-Metall
+#damit haben wir die positive Korrelation gezeigt
+
+
+###DL-Sektor: Hier ist es genau anders herum, Größe hat positiven Einfluss (+), aber 
+#DL und Größe korrelieren negativ miteinander (-) damit gilt:
+#Beta_DL wurde zu niedrig eingeschätzt 
+
+###1
+
+
+
+
